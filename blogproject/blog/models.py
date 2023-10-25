23f7f319ab83652c580e7f702b6924598df9ceeb
AhmadAleneziQ8 <alenezi.ahmad@cba.ku.edu.kw>
2023-10-25 12:18:09 +0300

    Update models.py

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#innharate Post with Model Super class
+#post its model (subclass)
+#sub class of model
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True ) #dont axceed 256 length !! 
+    #django give you PK named ID
+    slug = models.SlugField(max_length=200, unique=True )
+    body = models.TextField()
+    created_on = models.DateTimeFieldField(auto_now_add=True )
+    updated_on = models.DateTimeFieldField(auto_now=True )
+    status = models.IntegerField(default=0)
